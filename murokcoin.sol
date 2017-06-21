6597b501d0a61080cc8575c3404f121a95737d70
Matthew D. Huff <matthewhuff89@gmail.com>
2017-06-21 09:07:11 -0400

    Added mapping for balances, comments about what sol code is doing

--- murokcoin.sol
+++ murokcoin.sol
@@ -1,4 +1,11 @@
+/* This line specifies which version of the solidity compiler should be used */
+/* a pragma in CS tells how a compiler should process it's input */
+/* more details: pragmas are a form of directive, like #define in C */
 pragma solidity ^0.4.8;
+
 contract MurokCoin {
-  /// yeah this is happening :)
+  /* Creating an array with all balances, all of which are publicly accessible on the blockchain */
+  /* matt note: this is basically a hash table, or like a key-value object in JS */
+  /* the key is address, the value is an integer representing a balance */
+  mapping (address => uint256) public balanceOf;
 }
